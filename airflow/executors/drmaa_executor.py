a976aa95f26b2969289af30da1263ea9a4eaabf5
Karl Gutwin <karl@bioteam.net>
2017-06-30 10:14:37 -0400

    Add further DRMAA configuration

--- airflow/executors/drmaa_executor.py
+++ airflow/executors/drmaa_executor.py
@@ -3,9 +3,12 @@
 #
 
 import os
+import re
 
 import drmaa
 
+from airflow import configuration
+from airflow.utils.file import mkdirs
 from airflow.executors.base_executor import BaseExecutor
 from airflow.utils.state import State
 
@@ -52,21 +55,44 @@ class DrmaaExecutor(BaseExecutor):
         self.job_state = {}
 
     def execute_async(self, key, command, queue=None):
+        if queue is None:
+            if configuration.has_option('drmaa', 'default_queue'):
+                queue = configuration.get('drmaa', 'default_queue')
+            else:
+                queue = ""
         self.logger.info("[drmaa] queueing {key} through DRMAA, "
                          "queue={queue}".format(**locals()))
 
         jt = self.session.createJobTemplate()
-        jt.jobName = key[0]
-        jt.workingDirectory = os.getcwd()
-        jt.remoteCommand = '/bin/sh'
-        job_args = ['-c']
+        jt.jobName = re.sub(r'\W+', '', '_'.join(key))
+        jt.workingDirectory = configuration.get('core', 'airflow_home')
+        jt.nativeSpecification = self.make_native_spec(queue)
+        jt.joinFiles = False
+        log_base = os.path.join(
+            os.path.expanduser(configuration.get('core', 'base_log_folder')),
+            'drmaa_executor',
+            key[0]
+        )
+        if not os.path.exists(log_base):
+            mkdirs(log_base, 0o775)
+        jt.outputPath = os.path.join(log_base, "{dag}.{date}.out".format(
+            dag=key[1], date=key[2].isoformat()))
+        jt.errorPath = os.path.join(log_base, "{dag}.{date}.err".format(
+            dag=key[1], date=key[2].isoformat()))
+
+        if configuration.has_option('drmaa', 'wrapper_script'):
+            jt.remoteCommand = configuration.get('drmaa', 'wrapper_script')
+            job_args = []
+        else:
+            jt.remoteCommand = '/bin/sh'
+            job_args = ['-c']
         if isinstance(command, basestring):
             job_args.append(command)
         else:
             job_args.append(" ".join(command))
         jt.args = job_args
-        self.logger.info("[drmaa] {key} arguments: {job_args}".format(
-            key=key, job_args=job_args))
+        self.logger.info("[drmaa] {key} command: {cmd} {job_args}".format(
+            key=key, cmd=jt.remoteCommand, job_args=job_args))
 
         self.jobs[key] = self.session.runJob(jt)
         self.job_state[key] = State.QUEUED
@@ -115,3 +141,78 @@ class DrmaaExecutor(BaseExecutor):
             self.session.control(jobid, drmaa.JobControlAction.TERMINATE)
 
         self.session.exit()
+
+    def make_native_spec(self, queue):
+        ns = ""
+        if queue and configuration.has_option('drmaa', 'scheduler_type'):
+            sched_type = configuration.get('drmaa', 'scheduler_type').lower()
+            queue, cpu, mem, resources = (queue.split(':', 3) + [None] * 3)[:4]
+            if sched_type == 'sge':
+                ns = self.make_native_spec_sge(queue, cpu, mem, resources)
+            elif sched_type == 'pbs':
+                ns = self.make_native_spec_pbs(queue, cpu, mem, resources)
+            elif sched_type == 'slurm':
+                ns = self.make_native_spec_slurm(queue, cpu, mem, resources)
+            elif sched_type == 'lsf':
+                ns = self.make_native_spec_lsf(queue, cpu, mem, resources)
+            else:
+                warnings.warn('Scheduler type {} is not supported, ignoring '
+                              'queue specification'.format(sched_type))
+            
+        if configuration.has_option('drmaa', 'native_spec'):
+            ns += " " + configuration.get('drmaa', 'native_spec')
+
+        return ns
+
+    def make_native_spec_sge(self, queue, cpu, mem, resources):
+        ns = []
+        if queue:
+            ns.append('-q ' + queue)
+        if cpu:
+            if configuration.has_option('drmaa', 'sge_parallel_env'):
+                ns.append('-pe {} {}'.format(
+                    configuration.get('drmaa', 'sge_parallel_env'),
+                    cpu))
+            else:
+                warnings.warn('DRMAA setting sge_parallel_env is not set, '
+                              'ignoring queue CPU specification')
+        if mem:
+            ns.append('-l mem_free=' + mem)
+        if resources:
+            ns.append('-l ' + resources)
+        return " ".join(ns)
+
+    def make_native_spec_pbs(self, queue, cpu, mem, resources):
+        ns = []
+        if queue:
+            ns.append('-q ' + queue)
+        if cpu:
+            ns.append('-l ppn=' + cpu)
+        if mem:
+            ns.append('-l mem=' + mem)
+        if resources:
+            ns.append('-l other=' + resources)
+        return " ".join(ns)
+
+    def make_native_spec_slurm(self, queue, cpu, mem, resources):
+        ns = []
+        if queue:
+            ns.append('-p ' + queue)
+        if cpu:
+            ns.append('-n ' + cpu)
+        if mem:
+            ns.append('--mem-per-cpu=' + mem)
+        if resources:
+            ns.append('--gres=' + resources)
+        return " ".join(ns)
+
+    def make_native_spec_lsf(self, queue, cpu, mem, resources):
+        ns = []
+        if queue:
+            ns.append('-q ' + queue)
+        if cpu:
+            ns.append('-n ' + cpu)
+        if mem:
+            ns.append('-M ' + mem)
+        return " ".join(ns)
+                      
